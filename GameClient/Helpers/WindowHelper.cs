49c87b4cf15e4e2a7e79ef7af9d54b1add27a196
pablobitw <pabloobitiu@gmail.com>
2025-11-24 00:17:06 -0600

    add: Opcion Modo de pantalla Full/Window/WindowNoBorder

---
+++ GameClient/Helpers/WindowHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace GameClient.Helpers
+{
+    public static class WindowHelper
+    {
+        public static void CenterWindow(this Window window)
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            window.Left = (screenWidth - window.Width) / 2;
+            window.Top = (screenHeight - window.Height) / 2;
+        }
+    }
+}
--- GameClient/Views/OptionsPage.xaml.cs
+++ GameClient/Views/OptionsPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using GameClient; 
+using GameClient;
+using GameClient.Helpers; 
 
 namespace GameClient.Views
 {
@@ -10,26 +11,76 @@ namespace GameClient.Views
         public OptionsPage()
         {
             InitializeComponent();
+            this.Loaded += OptionsPage_Loaded;
+        }
+
+        private void OptionsPage_Loaded(object sender, RoutedEventArgs e)
+        {
             LoadCurrentSettings();
         }
 
         private void LoadCurrentSettings()
         {
-            // TODO: Cargar configuración guardada (UserPreferences o archivo local)
-            // Ejemplo:
-            // MusicSlider.Value = Settings.Default.MusicVolume;
-            // LanguageComboBox.SelectedIndex = ...
+            var mainWindow = Window.GetWindow(this) as GameMainWindow;
+
+            if (mainWindow != null)
+            {
+                if (mainWindow.WindowStyle == WindowStyle.None && mainWindow.WindowState == WindowState.Maximized)
+                {
+                    ScreenModeComboBox.SelectedIndex = 0;
+                }
+                else if (mainWindow.WindowStyle == WindowStyle.None && mainWindow.WindowState == WindowState.Normal)
+                {
+                    ScreenModeComboBox.SelectedIndex = 1;
+                }
+                else
+                {
+                    ScreenModeComboBox.SelectedIndex = 2;
+                }
+            }
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            ApplyVideoSettings();
+
             double musicVol = MusicSlider.Value;
             double sfxVol = SfxSlider.Value;
-            string screenMode = (ScreenModeComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
             string language = (LanguageComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
 
+            MessageBox.Show("¡Configuración aplicada correctamente!", "Opciones", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
 
-            MessageBox.Show("¡Configuración guardada correctamente!", "Opciones", MessageBoxButton.OK, MessageBoxImage.Information);
+        private void ApplyVideoSettings()
+        {
+            var mainWindow = Window.GetWindow(this) as GameMainWindow;
+            if (mainWindow == null) return;
+
+            int selectedMode = ScreenModeComboBox.SelectedIndex;
+
+            switch (selectedMode)
+            {
+                case 0:
+                    mainWindow.WindowStyle = WindowStyle.None;
+                    mainWindow.WindowState = WindowState.Maximized;
+                    mainWindow.ResizeMode = ResizeMode.NoResize;
+                    break;
+
+                case 1: 
+                    mainWindow.WindowStyle = WindowStyle.None;
+                    mainWindow.WindowState = WindowState.Normal;
+                    mainWindow.ResizeMode = ResizeMode.NoResize;
+                    mainWindow.Width = 1280;
+                    mainWindow.Height = 720;
+                    mainWindow.CenterWindow(); 
+                    break;
+
+                case 2: 
+                    mainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+                    mainWindow.WindowState = WindowState.Normal;
+                    mainWindow.ResizeMode = ResizeMode.CanResize;
+                    break;
+            }
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
